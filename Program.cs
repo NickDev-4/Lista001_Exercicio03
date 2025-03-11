11f732b4594131e62bd546372728edb44b3835c2
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-03-11 10:35:40 -0300

    Exercício3

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
